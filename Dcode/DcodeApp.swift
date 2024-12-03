67241a988e76decd49fc7415c410c55a83cee413
Kaneis Zontanos <ramim@bc.edu>
2024-12-03 18:59:50 -0500

    Initial Commit

---
+++ Dcode/DcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DcodeApp.swift
+//  Dcode
+//
+//  Created by Kaneis Zontanos on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
